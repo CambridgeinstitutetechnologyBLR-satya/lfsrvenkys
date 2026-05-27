8beddce502ded2a65449c18495c0fee00c09a011
Satya <satya.24ece@cambridge.edu.in>
2026-05-28 02:32:44 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import cocotb
+from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 @cocotb.test()
@@ -6,12 +7,18 @@ async def test_project(dut):
 
     dut._log.info("Starting verification...")
 
+    # Start clock
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 5)
 
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 2)
 
+    # Observe output
     for cycle in range(10):
         await ClockCycles(dut.clk, 1)
         dut._log.info(
